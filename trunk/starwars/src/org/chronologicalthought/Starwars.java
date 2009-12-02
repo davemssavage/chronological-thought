a7e1f5ed3f434b5c71fe8931210262286bd980b1
davemssavage <davemssavage@7be0d550-b8d5-11de-8dfb-43c0dffa5306>
2009-12-02 11:09:22 +0000

    remove dist and install targets

--- trunk/starwars/src/org/chronologicalthought/Starwars.java
+++ trunk/starwars/src/org/chronologicalthought/Starwars.java
@@ -9,93 +9,94 @@ import java.io.Reader;
 import java.net.URL;
 
 public class Starwars {
-	public void play() throws IOException, InterruptedException {
-		play(67);
-	}
-	
-	public void play(int frameLength) throws IOException, InterruptedException {
-		URL res = Starwars.class.getResource("/starwars.txt");
-		if ( res == null ) throw new IllegalStateException("Missing resource");
-		InputStream in = res.openStream();
-		try {
-			InputStreamReader reader = new InputStreamReader( new BufferedInputStream(in) );
-			render(reader, System.out, frameLength);
-		}
-		finally {
-			in.close();
-		}
-	}
-	
-	private static final char ANSI_ESC = 27;
-	private static final String CLEAR_SCREEN = ANSI_ESC + "[2J";
+  public void starwars() throws IOException, InterruptedException {
+    starwars(67);
+  }
 
-	private void render(Reader reader, PrintStream out, int frameLength) throws IOException, InterruptedException {
-		boolean slash = false;
-		int line = 0;
-		int runlen = 1;
-		StringBuilder buf = new StringBuilder();
-		for (;;) {
-			int r = reader.read();
-			if ( r == -1 ) break;
-			switch ( r ) {
-			case '\\':
-				if (slash) { 
-					buf.append('\\'); 
-					slash = false; 
-				}
-				else {
-					slash = true;
-				}
-				break;
-			case 'n':
-				if ( slash ) {
-					switch(line++) {
-					case 0:
-						try {
-							runlen = Integer.parseInt(buf.toString());
-						}
-						catch (NumberFormatException e) {
-							// # used as final marker
-							return;
-						}
-						buf.setLength(0);
-						break;
-					case 13:
-						out.print(CLEAR_SCREEN);
-						out.println(buf.toString());
-						buf.setLength(0);
-						Thread.sleep(runlen * frameLength);
-						line = 0;
-						break;
-					default:
-						buf.append('\n');
-						break;
-					}
-					slash = false; 
-				}
-				else {
-					buf.append('n');
-				}
-				break;
-			default:
-				slash = false;
-				buf.append((char) r);
-				break;
-			}
-			
-		}
-	}
+  public void starwars(int frameLength) throws IOException, InterruptedException {
+    URL res = Starwars.class.getResource("/starwars.txt");
+    if (res == null)
+      throw new IllegalStateException("Missing resource");
+    InputStream in = res.openStream();
+    try {
+      InputStreamReader reader = new InputStreamReader(new BufferedInputStream(
+          in));
+      render(reader, System.out, frameLength);
+    } finally {
+      in.close();
+    }
+  }
 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		switch( args.length ) {
-		case 1:
-			new Starwars().play(Integer.parseInt(args[0]));
-			break;
-		case 0:
-			new Starwars().play();
-			break;
-		default:
-			throw new IllegalArgumentException("Unexpected arguments");
-		}
-	}
+  private static final char ANSI_ESC = 27;
+  private static final String CLEAR_SCREEN = ANSI_ESC + "[2J";
+
+  private void render(Reader reader, PrintStream out, int frameLength)
+      throws IOException, InterruptedException {
+    boolean slash = false;
+    int line = 0;
+    int runlen = 1;
+    StringBuilder buf = new StringBuilder();
+    for (;;) {
+      int r = reader.read();
+      if (r == -1)
+        break;
+      switch (r) {
+      case '\\':
+        if (slash) {
+          buf.append('\\');
+          slash = false;
+        } else {
+          slash = true;
+        }
+        break;
+      case 'n':
+        if (slash) {
+          switch (line++) {
+          case 0:
+            try {
+              runlen = Integer.parseInt(buf.toString());
+            } catch (NumberFormatException e) {
+              // # used as final marker
+              return;
+            }
+            buf.setLength(0);
+            break;
+          case 13:
+            out.print(CLEAR_SCREEN);
+            out.println(buf.toString());
+            buf.setLength(0);
+            Thread.sleep(runlen * frameLength);
+            line = 0;
+            break;
+          default:
+            buf.append('\n');
+            break;
+          }
+          slash = false;
+        } else {
+          buf.append('n');
+        }
+        break;
+      default:
+        slash = false;
+        buf.append((char) r);
+        break;
+      }
+
+    }
+  }
+
+  public static void main(String[] args) throws IOException,
+      InterruptedException {
+    switch (args.length) {
+    case 1:
+      new Starwars().starwars(Integer.parseInt(args[0]));
+      break;
+    case 0:
+      new Starwars().starwars();
+      break;
+    default:
+      throw new IllegalArgumentException("Unexpected arguments");
+    }
+  }
 }
